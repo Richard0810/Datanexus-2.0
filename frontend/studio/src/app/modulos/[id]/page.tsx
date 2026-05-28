1f927d4471933ff3649b8f051b287a51b6b07288
Richard Perez Sofan <richard2003prezsofan@gmail.com>
2026-05-28 03:54:47 +0000

    fix(build): Mueve la directiva 'use client' a la primera línea

--- frontend/studio/src/app/modulos/[id]/page.tsx
+++ frontend/studio/src/app/modulos/[id]/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 "use client";
+import React from 'react';
 
 import { useEffect, useState, use, useRef } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
